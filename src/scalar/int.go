9a91e3bd1e1d173cdc97cc36419a448f1095c72a
reus <reusee@gmail.com>
2025-11-21 01:00:47 +0800

    scalar: implement identifiable interfaces for Int and String

--- src/scalar/int.go
+++ src/scalar/int.go
@@ -11,9 +11,28 @@ type Int int
 var _ core.Expression = Int(0)
 
 func (i Int) CanApply(transform any) bool {
+	switch transform.(type) {
+	case core.ToLogicalID, core.ToPhysicalID, core.ToCanonicalID:
+		return true
+	}
 	return false
 }
 
 func (i Int) String() string {
 	return strconv.Itoa(int(i))
+}
+
+func (i Int) LogicalID() core.Identifier {
+	return core.Identifier{
+		Kind: "int",
+		Key:  strconv.Itoa(int(i)),
+	}
+}
+
+func (i Int) PhysicalID() core.Identifier {
+	return i.LogicalID()
+}
+
+func (i Int) CanonicalID(*core.Context) (core.Identifier, error) {
+	return i.LogicalID(), nil
 }
--- src/scalar/string.go
+++ src/scalar/string.go
@@ -7,9 +7,28 @@ type String string
 var _ core.Expression = String("")
 
 func (s String) CanApply(transform any) bool {
+	switch transform.(type) {
+	case core.ToLogicalID, core.ToPhysicalID, core.ToCanonicalID:
+		return true
+	}
 	return false
 }
 
 func (s String) String() string {
 	return string(s)
+}
+
+func (s String) LogicalID() core.Identifier {
+	return core.Identifier{
+		Kind: "string",
+		Key:  string(s),
+	}
+}
+
+func (s String) PhysicalID() core.Identifier {
+	return s.LogicalID()
+}
+
+func (s String) CanonicalID(*core.Context) (core.Identifier, error) {
+	return s.LogicalID(), nil
 }
