dec260b0097b4c9a37c2cced83abc471f0eaf013
reus <reusee@gmail.com>
2025-11-21 10:28:21 +0800

    scalar: implement Ordered[Int] for Int

--- src/scalar/int.go
+++ src/scalar/int.go
@@ -1,6 +1,7 @@
 package scalar
 
 import (
+	"cmp"
 	"strconv"
 
 	"github.com/ArborDB/arbordb/src/core"
@@ -22,6 +23,8 @@ func (i Int) String() string {
 	return strconv.Itoa(int(i))
 }
 
+var _ core.LogicalIdentifiable = Int(0)
+
 func (i Int) LogicalID() core.Identifier {
 	return core.Identifier{
 		Kind: "int",
@@ -29,10 +32,20 @@ func (i Int) LogicalID() core.Identifier {
 	}
 }
 
+var _ core.PhysicalIdentifiable = Int(0)
+
 func (i Int) PhysicalID() core.Identifier {
 	return i.LogicalID()
 }
 
+var _ core.CanonicalIdentifiable = Int(0)
+
 func (i Int) CanonicalID(*core.Context) (core.Identifier, error) {
 	return i.LogicalID(), nil
+}
+
+var _ core.Ordered[Int] = Int(0)
+
+func (i Int) Compare(to Int) int {
+	return cmp.Compare(i, to)
 }
