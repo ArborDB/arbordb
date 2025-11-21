f9e80299b6f39735de53821b3226a158238b7812
reus <reusee@gmail.com>
2025-11-21 10:29:57 +0800

    scalar: implement Ordered[String] for String

--- src/scalar/string.go
+++ src/scalar/string.go
@@ -1,6 +1,10 @@
 package scalar
 
-import "github.com/ArborDB/arbordb/src/core"
+import (
+	"cmp"
+
+	"github.com/ArborDB/arbordb/src/core"
+)
 
 type String string
 
@@ -18,6 +22,8 @@ func (s String) String() string {
 	return string(s)
 }
 
+var _ core.LogicalIdentifiable = String("")
+
 func (s String) LogicalID() core.Identifier {
 	return core.Identifier{
 		Kind: "string",
@@ -25,10 +31,20 @@ func (s String) LogicalID() core.Identifier {
 	}
 }
 
+var _ core.PhysicalIdentifiable = String("")
+
 func (s String) PhysicalID() core.Identifier {
 	return s.LogicalID()
 }
 
+var _ core.CanonicalIdentifiable = String("")
+
 func (s String) CanonicalID(*core.Context) (core.Identifier, error) {
 	return s.LogicalID(), nil
+}
+
+var _ core.Ordered[String] = String("")
+
+func (s String) Compare(to String) int {
+	return cmp.Compare(s, to)
 }
